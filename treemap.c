afe6ed4a9c021140f75ed3ef14d069798e98c524
Replit user <>
2023-09-14 19:14:38 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -38,7 +38,7 @@ TreeNode * createTreeNode(void* key, void * value) {
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
     TreeMap * mapa = (TreeMap *)malloc(sizeof(TreeMap));
-    if (TreeMap == NULL){
+    if (mapa == NULL){
         return NULL;  
     } 
     mapa->root = NULL;
